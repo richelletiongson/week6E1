9bca90ca132971d5e781c6f443c6551e0ec7b0d3
Richelle Tiongson <richelletiongson@Richelles-MacBook-Air.local>
2024-10-13 16:54:29 -0700

    final

--- classE2.js
+++ classE2.js
@@ -2,3 +2,34 @@ const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
   });
+
+  function GuessingGame(answer, guess) {
+    if (guess > answer) {
+        console.log("Too big!");
+    } else if (guess < answer) {
+        console.log("Too small!");
+    } else {
+        console.log("You win!");
+    }
+  }
+  
+  readline.question("What is the answer?", (userAnswer) => {
+  
+   
+    function StartGame() {
+  
+
+      readline.question("What is your guess? ", (userGuess) => {
+        
+        GuessingGame();
+  
+        if (userGuess === "quit") {
+          readline.close();
+        } else {
+          StartGame();
+        }
+      });
+    }
+  
+    StartGame();
+  });
