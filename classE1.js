8b8d757a86073a7ce4ca4427862764b68e29e7c9
Richelle Tiongson <richelletiongson@Richelles-MacBook-Air.local>
2024-10-13 16:16:09 -0700

    final

--- classE1.js
+++ classE1.js
@@ -2,3 +2,26 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
   });
+
+  function CheckDrinkingAge(age){
+    if (age <13) {
+      console.log("Leave!");
+    } else if (age >= 13 && age < 19) {
+      console.log("Grow up to be 19.")
+    } else if (age >= 19 && age <= 50) {
+      console.log("Drink away!")
+    } else if (age > 50 && age <=70) {
+      console.log("Watch your health.")
+    } else if (age > 70) {
+      console.log("Watch you health and life.")
+    } else {
+      console.log ("Invalid age.")
+    }
+  }
+
+  readline.question('How old are you? ', userAge => {
+  
+    CheckDrinkingAge ();
+    
+      readline.close();
+    });
